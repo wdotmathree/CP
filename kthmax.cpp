daffab62c401213f15f9f67a23d967f7cbc10ed0
wdotmathree <wdotmathree@gmail.com>
2022-08-26 22:24:15 -0600

    Add simple IO optimizations

--- kthmax.cpp
+++ kthmax.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	int k, n;
 	cin >> k >> n;
 	int a[n];
--- uva11057.cpp
+++ uva11057.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	while (true) {
 		int N;
 		cin >> N;
