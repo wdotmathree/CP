78c88ec1799ca24db08edf214a566f6fad7e1a04
wdotmathree <wdotmathree@gmail.com>
2022-08-29 22:36:17 -0600

    Add explanation for Discounts

--- cf1132b.cpp
+++ cf1132b.cpp
@@ -1,3 +1,15 @@
+/*
+	Sort the array (a) of costs of chocolate bars in descending order
+	Calculate the sum of the cost of all chocolate bars
+	The most optimal way to use a coupon is to purchase the most expensive items with that coupon
+	In that case, the least expensive of the group will be given for free
+	For example:
+		If you have a coupon that lets you buy 3 and pay for 2, the most optimal way to use it would be to buy the most
+		Expensive 3 chocolate bars and the 3rd most expensive one will be given for free
+	With this observation, we know that the optimal price for each coupon is the sum of all chocolate bars minus the cost of
+	The q-th most expensive one, which will be in index q - 1 due to the list being sorted in descending order
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
