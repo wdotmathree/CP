b280a6e7f0a23f4a58affb0a0f2bf8cbe62b2d69
wdotmathree <wdotmathree@gmail.com>
2022-08-29 22:26:27 -0600

    Add template

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	vector<int> a;
+	a.reserve(n);
+	for (int i = 0; i < n; i++) {
+		a.push_back(n);
+	}
+	return 0;
+}
