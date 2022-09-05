a67135ebe92654a8edcef8ccd4d7ba020366a579
wdotmathree <wdotmathree@gmail.com>
2022-09-04 23:07:04 -0600

    Remove redundances in solution for Two Sets

--- cses1092.cpp
+++ cses1092.cpp
@@ -26,10 +26,10 @@ int main() {
 		}
 		// If the array can't be split that way, it can't be split (I can't prove this mathematically so you'll just have to trust me)
 		// So check if the array can be split that way. To do that we use Gauss' formula for sum of an arithmetic series
-		// We take the sum of everything up to and including the right bound and subtract the sum of everything up to but not including the left boundary.
 		// After doing that, we should get the sum of everything between (and including) the left and right boundaries
-		// We double that number and compare it with the sum of the array. If they are equal, that's our solution, and if not, there is no solution
-		if ((long long)(((rb + 1) * (rb / 2.)) - (long long)(lb * ((lb - 1) / 2.))) * 2 != (long long)((n + 1) * (n / 2.))) {
+		// We double that number (the double cancels with the half from Gauss' formula) and compare it with the sum of the array.
+		// If they are equal, that's our solution, and if not, there is no solution
+		if ((lb + rb) * (rb - lb + 1) != (long long)((n + 1) * (n / 2.))) {
 			cout << "NO\n";
 		} else {
 			cout << "YES\n" << ceil(n / 2.) << '\n';
