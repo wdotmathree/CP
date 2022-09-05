5c7c78cda96aa9be061bfa21daa7f86dfc09f01d
wdotmathree <wdotmathree@gmail.com>
2022-09-04 22:55:43 -0600

    Add explanation for Two Sets

--- cses1092.cpp
+++ cses1092.cpp
@@ -7,17 +7,28 @@ int main() {
 	cout.tie(0);
 	int n;
 	cin >> n;
+	// If the sum of the array is an odd number, it is not possible to split it into 2 equal sets
 	if ((int)((n + 1) * (n / 2.)) % 2) {
 		cout << "NO\n";
 	} else {
+		// Define left and right bounds for first set (inclusive on both sides)
 		long long lb, rb;
 		if (n % 2) {
+			// If the array is odd, the first element in the set will be ceil(n / 4)
 			lb = ceil(n / 4.);
-			rb = ceil(n / 2.) + ceil(n / 4.) - 1;
+			// And the last element will be n - lb
+			rb = n - lb;
 		} else {
+			// If the array is even, the first element in the set will be n / 4 + 1
 			lb = n / 4 + 1;
-			rb = n / 2 + n / 4;
+			// And the last element will be n - lb + 1
+			rb = n - lb + 1;
 		}
+		// If the array can't be split that way, it can't be split (I can't prove this mathematically so you'll just have to trust me)
+		// So check if the array can be split that way. To do that we use Gauss' formula for sum of an arithmetic series
+		// We take the sum of everything up to and including the right bound and subtract the sum of everything up to but not including the left boundary.
+		// After doing that, we should get the sum of everything between (and including) the left and right boundaries
+		// We double that number and compare it with the sum of the array. If they are equal, that's our solution, and if not, there is no solution
 		if ((long long)(((rb + 1) * (rb / 2.)) - (long long)(lb * ((lb - 1) / 2.))) * 2 != (long long)((n + 1) * (n / 2.))) {
 			cout << "NO\n";
 		} else {
